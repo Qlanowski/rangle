ed40b5663be7d68ffe0f216d40501229acf278b6
Karol Ulanowski <ulanowskikarol@gmail.com>
2021-04-24 13:10:06 +0200

    update cost function

--- cost_functions.py
+++ cost_functions.py
@@ -1,17 +1,21 @@
-#%%
+# %%
 import tensorflow as tf
+
 
 @tf.function
 def mse(y_actual, y_pred):
-    return tf.reduce_mean(tf.square(y_actual - y_pred))
+    valid = tf.cast(tf.math.reduce_max(a, axis=(2, 3)) > 0, dtype=tf.float32)
+    valid_mask = tf.reshape(valid, [tf.shape(y_pred)[0], 1, 1, tf.shape(valid)[-1]])
+    return tf.reduce_mean(tf.square(y_actual - y_pred) * valid_mask)
 
-#%%
+
+# %%
 if __name__ == "__main__":
-   
+
     a = tf.constant([
-        [1,2],
-        [1,2]
-    ],dtype=tf.float32)
+        [1, 2],
+        [1, 2]
+    ], dtype=tf.float32)
     b = tf.math.multiply(0.5, a)
 
 
