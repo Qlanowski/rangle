2b5c725144cbd5d425bb2027bcf18d308fdaf590
Karol Ulanowski <ulanowskikarol@gmail.com>
2021-04-25 18:00:52 +0200

    add regularization to ef lite

--- nets/efficient_net_lite.py
+++ nets/efficient_net_lite.py
@@ -3,10 +3,14 @@ from tensorflow.keras.regularizers import l2
 from tensorflow.keras import layers, Model
 from tensorflow.keras.applications import ResNet50, ResNet101, ResNet152
 
+from .simple_baseline import add_regularization
+
 def EfficientNetLite(size):
     regularizer = l2(1e-5)
 
     backbone = tf.keras.models.load_model(f'EFLiteModels/L{size}.h5')
+    backbone = add_regularization(backbone, regularizer)
+
     x = backbone.layers[-4].output
     for i in range(3):
         x = layers.Conv2DTranspose(
